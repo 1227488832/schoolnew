afa3d69ba03524644e2c8c8dffad952b01a42ae0
杨朋 <1227488832@qq.com>
2022-11-15 02:41:08 +0800

    token

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include "quechao_auth.php" ?><!doctype html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="maximum-scale=1,minimum-scale=1,user-scalable=0,width=device-width,initial-scale=1"/><link rel="icon" href="favicon.ico"><title>schoolactivities</title><script src="rem.js"></script><script>window.onload = function () {
       console.log(userInfo);
-      window.sessionStorage.setItem('userInfo',userInfo)
-    }</script><script defer="defer" src="js/chunk-vendors.17c8c711.js"></script><script defer="defer" src="js/app.57dc3178.js"></script><link href="css/chunk-vendors.c1cc1c89.css" rel="stylesheet"><link href="css/app.0d73a1df.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but schoolactivities doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>
+      let user = window.sessionStorage.setItem('userInfo',userInfo)
+      console.log(user);
+    }</script><script defer="defer" src="js/chunk-vendors.17c8c711.js"></script><script defer="defer" src="js/app.5ff65ca3.js"></script><link href="css/chunk-vendors.c1cc1c89.css" rel="stylesheet"><link href="css/app.0d73a1df.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but schoolactivities doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>
