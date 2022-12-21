b1c7fbfa02bd5b9a6ce669c5a57fc2054f99a526
杨朋 <1227488832@qq.com>
2022-12-21 17:31:38 +0800

    统计

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include "quechao_auth.php" ?> <!doctype html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="maximum-scale=1,minimum-scale=1,user-scalable=0,width=device-width,initial-scale=1"/><link rel="icon" href="favicon.ico"><title>雀巢咖啡 提醒每一天</title><script src="rem.js"></script><script src="jweixin-1.6.0.js"></script><script>window.onload = function () {
+      
     // const userInfo = JSON.parse('{"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7Im9wZW5pZCI6Im9rZ3dBdUFXcDVvcDdYQ1NWU19vLUZtTWRlYk0iLCJvcmlnaW4iOiJxdWVfY2hhbyIsInRpbWVTdGFtcCI6MTY2ODQ3OTUwNX0sImlzcyI6Imh0dHBzOlwvXC9hZC53ZnNpZGFvLmNvbSIsImF1ZCI6Im1yLWtvbmciLCJpYXQiOjE2Njg0Nzk1MDUsIm5iZiI6MTY2ODQ3OTUwNSwiZXhwIjoxNjcxMDcxNTA1fQ.hsZpOduQAAXhwnfsdoQgq066h8h-llOctY5zadku9oc","nickname":"左右","headimgurl":"https://thirdwx.qlogo.cn/mmopen/vi_32/X6goVqq7eXeywkrhTpr3zVVe3ZOBibESpGNZVgDbJf2HPMTpmDwO84FVfdbCqTmETwZO9SeibCKPgLogdjFGbj7w/132"}')
       console.log('qss');
       sessionStorage.setItem('userInfo', JSON.stringify(userInfo))
