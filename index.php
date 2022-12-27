7ef79d826bd17e10d463ed2ac7c84a43cf88afa6
‘1227488832’ <1227488832@qq.com>
2022-12-27 12:20:59 +0800

    统计页面次数

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
-<?php include "quechao_auth.php" ?> <!doctype html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="maximum-scale=1,minimum-scale=1,user-scalable=0,width=device-width,initial-scale=1"/><link rel="icon" href="favicon.ico"><title>雀巢咖啡 提醒每一天</title><script src="rem.js"></script><script src="jweixin-1.6.0.js"></script><script>window.onload = function () {
-      console.log('日期修改');
-    // const userInfo = JSON.parse('{"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7Im9wZW5pZCI6Im9rZ3dBdUFXcDVvcDdYQ1NWU19vLUZtTWRlYk0iLCJvcmlnaW4iOiJxdWVfY2hhbyIsInRpbWVTdGFtcCI6MTY2ODQ3OTUwNX0sImlzcyI6Imh0dHBzOlwvXC9hZC53ZnNpZGFvLmNvbSIsImF1ZCI6Im1yLWtvbmciLCJpYXQiOjE2Njg0Nzk1MDUsIm5iZiI6MTY2ODQ3OTUwNSwiZXhwIjoxNjcxMDcxNTA1fQ.hsZpOduQAAXhwnfsdoQgq066h8h-llOctY5zadku9oc","nickname":"左右","headimgurl":"https://thirdwx.qlogo.cn/mmopen/vi_32/X6goVqq7eXeywkrhTpr3zVVe3ZOBibESpGNZVgDbJf2HPMTpmDwO84FVfdbCqTmETwZO9SeibCKPgLogdjFGbj7w/132"}')
+<?php include "quechao_auth.php" ?> <!doctype html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="maximum-scale=1,minimum-scale=1,user-scalable=0,width=device-width,initial-scale=1"/><link rel="icon" href="favicon.ico"><title>雀巢咖啡 提醒每一天</title><script src="rem.js"></script><script src="jweixin-1.6.0.js"></script><script>var _hmt = _hmt || [];
+    window._hmt = _hmt; // 必须把_hmt挂载到window下，否则找不到
+    (function () {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?9e14499d2cf8cab26c33f49108e7d087";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+    })();</script><script>window.onload = function () {
+      console.log('合成提示');
+      // const userInfo = JSON.parse('{"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7Im9wZW5pZCI6Im9rZ3dBdUFXcDVvcDdYQ1NWU19vLUZtTWRlYk0iLCJvcmlnaW4iOiJxdWVfY2hhbyIsInRpbWVTdGFtcCI6MTY3MjExMTQ2Nn0sImlzcyI6Imh0dHBzOlwvXC9hZC53ZnNpZGFvLmNvbSIsImF1ZCI6Im1yLWtvbmciLCJpYXQiOjE2NzIxMTE0NjYsIm5iZiI6MTY3MjExMTQ2NiwiZXhwIjoxNjc0NzAzNDY2fQ.WljEen4NOFvRs3AUcpcXBIHL9wwVPzqNjFQZZ7ZTiZI","nickname":"左右","headimgurl":"https:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/X6goVqq7eXeywkrhTpr3zVVe3ZOBibESpGNZVgDbJf2HPMTpmDwO84FVfdbCqTmETwZO9SeibCKPgLogdjFGbj7w\/132"}')
       sessionStorage.setItem('userInfo', JSON.stringify(userInfo))
       console.log(window.sessionStorage.getItem("userInfo"));
-    }</script><script defer="defer" src="js/chunk-vendors.df45d7b6.js"></script><script defer="defer" src="js/app.d3bafc7d.js"></script><link href="css/chunk-vendors.414ccacb.css" rel="stylesheet"><link href="css/app.6c853906.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but schoolactivities doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>
+    }</script><script defer="defer" src="js/chunk-vendors.df45d7b6.js"></script><script defer="defer" src="js/app.d52aea9c.js"></script><link href="css/chunk-vendors.414ccacb.css" rel="stylesheet"><link href="css/app.6c853906.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but schoolactivities doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>
